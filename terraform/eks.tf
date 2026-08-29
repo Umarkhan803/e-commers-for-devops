ea47ece4bbf10085b348513b665d527c2280a5ec
Umarkhan803 <umar.khan@quantumquest.in>
2026-08-30 01:08:22 +0530

    helm

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -43,7 +43,9 @@ resource "aws_eks_cluster" "main" {
 
   }
   access_config {
-    authentication_mode = "API_AND_CONFIG_MAP"
+    authentication_mode                         = "API_AND_CONFIG_MAP"
+    bootstrap_cluster_creator_admin_permissions = true
+
   }
 
   depends_on = [aws_iam_role_policy_attachment.eks_cluster_policy]
